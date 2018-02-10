6da8bd692a1e33e45b14cc0f7eb2b29a07addeb3
Jeremy Daly <jeremy@jeremydaly.com>
2018-02-09 22:00:48 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Serverless API Sample Project
+
+This is a sample [Serverless](https://serverless.com) project that creates a serverless API using the [Serverless](https://serverless.com) framework and [lambda-api](https://github.com/jeremydaly/lambda-api), a lightweight Node.js API router for AWS Lambda and API Gateway.
+
+This project is a companion to the post [How To: Build a Serverless API with Serverless, AWS Lambda and lambda-api](https://www.jeremydaly.com/build-serverless-api-serverless-aws-lambda-lambda-api) at [JeremyDaly.com](https://www.jeremydaly.com).
+
+This project is free to use as a starting point for building your Serverless APIs.
+
+## Requirements
+This project requires the installation of the [Serverless](https://serverless.com) framework:
+
+```bash
+npm install -g serverless
+```
+More details at: https://serverless.com/learn/quick-start/
+
+This project is also dependent on [lambda-api](https://github.com/jeremydaly/lambda-api) and [serverless-stage-manager](https://github.com/jeremydaly/serverless-stage-manager). Both can be installed by running the following in the cloned project folder:
+
+```bash
+npm install
+```
---
+++ handler.js
@@ -0,0 +1,124 @@
+'use strict'
+
+/**
+ * Sample serverless API using Serverless framework and lambda-api
+ * @author Jeremy Daly <jeremy@jeremydaly.com>
+ * @version 1.0.0
+ * @license MIT
+ */
+
+
+// Require and init API router module
+const API = require('lambda-api')
+const app = new API({ version: 'v1.0', base: 'v1' })
+
+
+//----------------------------------------------------------------------------//
+// Define Middleware
+//----------------------------------------------------------------------------//
+
+  // Add CORS Middleware
+  app.use(function(req,res,next) {
+
+    // Add CORS headers for every request
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+
+    // Call next to continue processing
+    next()
+  })
+
+
+  // Add Authorization Middleware
+  app.use(function(req,res,next) {
+
+    // Check for Authorization headers
+    if (req.headers.Authorization) {
+      // Attempt to parse the Bearer token
+      let token = req.headers.Authorization.replace(/^Bearer/,'').trim()
+      // Set the token in the request scope
+      req.token = token
+      // Do some checking here to make sure it is valid (set an auth flag)
+      req.auth = true
+    }
+
+    // Call next to continue processing
+    next()
+  })
+
+//----------------------------------------------------------------------------//
+// Build API routes
+//----------------------------------------------------------------------------//
+
+  // Get
+  app.get('/posts', function(req,res) {
+    // Send the response
+    res.status(200).json({
+      status: 'ok',
+      version: req.version,
+      auth: req.auth,
+      body: req.body,
+      query: req.query
+    })
+  })
+
+  // Post
+  app.post('/posts', function(req,res) {
+    // Send the response
+    res.status(200).json({
+      status: 'ok',
+      version: req.version,
+      auth: req.auth,
+      body: req.body,
+      query: req.query
+    })
+  })
+
+  // Put
+  app.put('/posts/:post_id', function(req,res) {
+    // Send the response
+    res.status(200).json({
+      status: 'ok',
+      version: req.version,
+      auth: req.auth,
+      body: req.body,
+      query: req.query,
+      params: req.params
+    })
+  })
+
+
+  // Delete
+  app.delete('/posts/:post_id', function(req,res) {
+    // Send the response
+    res.status(200).json({
+      status: 'ok',
+      version: req.version,
+      auth: req.auth,
+      params: req.params
+    })
+  })
+
+
+  // Default Options for CORS preflight
+  app.options('/*', function(req,res) {
+    res.status(200).json({})
+  })
+
+
+
+//----------------------------------------------------------------------------//
+// Main router handler
+//----------------------------------------------------------------------------//
+module.exports.router = (event, context, callback) => {
+
+  // !!!IMPORTANT: Set this flag to false, otherwise the lambda function
+  // won't quit until all DB connections are closed, which is not good
+  // if you want to freeze and reuse these connections
+  context.callbackWaitsForEmptyEventLoop = false
+
+  // Run the request
+  app.run(event,context,callback)
+
+} // end router handler
